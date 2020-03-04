00251582e4b693339f1895ce3253be09717a5093
Felipe <felipe.ascencio@virginiogomez.cl>
2020-03-04 20:29:02 -0300

    server 32

--- server.js
+++ server.js
@@ -47,10 +47,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
 const port = process.env.PORT || 3000;
