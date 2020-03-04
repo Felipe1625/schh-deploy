dadde79dcf07eee71da6d0ff42dc23c2cd1e1a15
Felipe <felipe.ascencio@virginiogomez.cl>
2020-03-04 20:54:24 -0300

    server 36

--- server.js
+++ server.js
@@ -41,9 +41,23 @@ class Server {
         this.app.use('/api/tiempo', TiempoRoutes_1.default);
     }
     start() {
-        app.route('/*', function(req,res) {
-            res.redirect(__dirname + '/dist/index.html')
-          })
+        const express = require('express');
+const http = require('http')
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
 
         
     }
