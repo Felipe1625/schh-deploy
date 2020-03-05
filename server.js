599d2891adc2ab99e25e79321b459c1eb1109b38
Felipe <felipe.ascencio@virginiogomez.cl>
2020-03-04 21:05:06 -0300

    server 38

--- server.js
+++ server.js
@@ -41,23 +41,55 @@ class Server {
         this.app.use('/api/tiempo', TiempoRoutes_1.default);
     }
     start() {
-        const express = require('express');
-const http = require('http')
+        // Get dependencies
+const express = require('express');
 const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+var cool = require('cool-ascii-faces');
 
+//var db = require('./db/connect.js');
+// Get our API routes
+//const api = require('./server/routes/api');
+var appRoutes = require('routes/indexRoutes');
 const app = express();
 
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/client/index.html'));
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+app.use('/', appRoutes)
+// Set our api routes
+//app.use('/api', api);
 
-const port = process.env.PORT || 3000;
+// Catch all other routes and return the index file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '4001';
 app.set('port', port);
 
+/**
+ * Create HTTP server.
+ */
 const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
 
         
     }
