3ec0a6e529a5215d30bb9c3e966895155b6ef998
Felipe <felipe.ascencio@virginiogomez.cl>
2020-03-04 21:54:14 -0300

    server 57

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ class Server {
     app.use(express.static(path.join(__dirname, "client")));
 
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname + "/client/index.html"));
+      res.sendFile(path.join(__dirname + "/client/dist/index.html"));
     });
 
     const port = process.env.PORT || 3000;
